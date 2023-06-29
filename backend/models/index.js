c448ddb7fc56a04b38c926bb139a824f7dc5197c
hart <100047482+loyallhart@users.noreply.github.com>
2023-06-29 17:07:33 -0400

    backend server and query

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,1 +1,2 @@
-//
+const User = require('./user');
+module.exports = {User}
---
+++ backend/schemas/resolvers.js
@@ -0,0 +1,10 @@
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+          return User.find({});
+        }, 
+    }
+}
+module.exports = resolvers;
