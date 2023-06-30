a730c798cd12bae9987e9672f384a7562c5c214c
LaChaznie Moore <lachazniemoore@LaChaznies-MacBook-Air.local>
2023-06-29 22:12:24 -0400

    added chakra-ui

---
+++ clients/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from 'react'
+
+
+function App() {
+  return (
+    <ChakraProvider>
+      <TheRestOfYourApplication />
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ clients/src/component/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
+const Login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
