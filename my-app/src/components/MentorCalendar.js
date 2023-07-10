7c8aef6ab586d1b4763e56d37e86c00555ec9321
janevalen00 <bluevalenz@blues-mbp.attlocal.net>
2023-07-10 14:30:56 -0400

    Feat: add calendly to mentorCalendar

--- my-app/src/components/MentorCalendar.js
+++ my-app/src/components/MentorCalendar.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { InlineWidget, useCalendlyEventListener } from 'react-calendly';
 function MentorCalendar() {
-  return <div>MentorCalendar</div>;
+  useCalendlyEventListener({
+    onProfilePageViewed: () => console.log('onProfilePageViewed'),
+    onDateAndTimeSelected: () => console.log('onDateAndTimeSelected'),
+    onEventTypeViewed: () => console.log('onEventTypeViewed'),
+    onEventScheduled: e => console.log(e.data.payload),
+  });
+
+  return (
+    <div className="App">
+      <InlineWidget url="https://calendly.com/your_scheduling_page" />
+    </div>
+  );
 }
 export default MentorCalendar;
